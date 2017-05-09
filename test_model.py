4131ee5e0a52aa821b416f707ec8fb5fc637241d
Scrumilation <rod.junger@hotmail.com>
2017-05-09 01:11:19 -0300

    Actually send the input to vjoy, it works with x360ce!

--- test_model.py
+++ test_model.py
@@ -15,6 +15,9 @@ device.data.wAxisY = center
 device.data.wAxisZ = center
 device.data.wAxisXRot = center
 device.data.wAxisYRot = center
+device.data.wAxisZRot = center
+device.data.lButtons = 0
+
 device.update()
 
 print("Starting!")
@@ -29,5 +32,8 @@ while True:
     image = screenshoter.grab_screenshot()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.resize(image[:][200:464], (200, 66))
-    prediction = model.predict([image.reshape(200, 66, 1)])
-    print(prediction)
+    prediction = model.predict([image.reshape(200, 66, 1)])[0]
+
+    device.data.wAxisX = int(((prediction[0] / 2) + 0.5) * 32767) # Evil floating point bit level hacking
+    device.data.wAxisZRot = int(((((prediction[1] * -1) / 2) + 0.5) * 65535)) # What the fuck
+    device.update()
